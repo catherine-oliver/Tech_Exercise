24502af3845f622204e861060ad1f11c4bd826e6
Ryan <rchapman2000@gmail.com>
2021-06-16 01:27:16 -0500

    Tech Exercise 4 - Committing to Ryan's branch

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from .views import addPage
 from .views import addTask
+from .views import ryanBranchView
 
 urlpatterns = [
     path('', addPage, name='addPage'),
-    path('add-task/', addTask, name="addTask")
+    path('add-task/', addTask, name="addTask"),
+    path('ryan-branch/', ryanBranchView, name='ryanBranch')
 ]
