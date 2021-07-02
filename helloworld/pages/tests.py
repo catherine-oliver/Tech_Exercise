e08408b9b731500e99ba9d81dafe4dc42057a887
Ryan <rchapman2000@gmail.com>
2021-07-01 19:04:33 -0500

    Added Test for Add Functionality

--- helloworld/pages/tests.py
+++ helloworld/pages/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+
+class AddFunctionalityTest(LiveServerTestCase):
+
+    def testAdd(self):
+        selenium = webdriver.Chrome()
+        selenium.get("http://localhost:8000/")
+
+        task_input = selenium.find_element_by_id('task_name')
+        submit = selenium.find_element_by_id('first_submit')
+
+        task_input.send_keys("Selenium Test")
+
+        submit.send_keys(Keys.RETURN)
+
+        assert "Selenium Test" in selenium.page_source
